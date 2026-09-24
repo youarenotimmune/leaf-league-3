317580134eb8ddd65bd15079e1ef00414b6857c1
youarenotimmune <adeelt@gmail.com>
2026-09-25 04:43:49 +0500

    Refresh cache after persistence regression fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'leaf-league-iii-sw-v5';
+const VERSION = 'leaf-league-iii-sw-v6';
 const CORE = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
